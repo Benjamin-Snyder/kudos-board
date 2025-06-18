f98282a3ad19b913b75e074dfd75a5f2006c4104
Ben <snyderben598@gmail.com>
2025-06-18 11:31:11 -0700

    [KudoBoard][Backend] complete all endpoints

--- back-end/api/board-model-prisma.js
+++ back-end/api/board-model-prisma.js
@@ -5,15 +5,15 @@ const prisma = new PrismaClient();
 
 
 module.exports = {
-async find(where) {
+async findBoards(where) {
     // GET http://localhost:5432/api/boards?type=Recent
     // SELECT * FROM "Board" WHERE type='Recent';
-    const boards = await prisma.board.findMany({ where });
+    const boards = await prisma.board.findMany({where});
     return boards;
 
 },
 
-async findById(id) {
+async findBoardById(id) {
     // GET http://localhost:5432/api/boards/1
     // SELECT * FROM "Board" WHERE id = 1;
     const board = await prisma.board.findUnique({ where: { id } });
@@ -21,7 +21,7 @@ async findById(id) {
 
 },
 
-async create(newBoard) {
+async createBoard(newBoard) {
     // POST http://localhost:5432/api/boards/1 { title: "Vibes", author:"Ben" ,type: "Inspiration", ... }
     // INSERT INTO "Board"  VALUES ;
     const created = await prisma.board.create({ data: newBoard });
@@ -29,7 +29,7 @@ async create(newBoard) {
 
 },
 
-async update(id, changes) {
+async updateBoard (id, changes) {
     // PUT http://localhost:5432/api/boards/1
     // UPDATE "Pet" SET adopted = true WHERE id = 1;
     const updated = await prisma.board.update({ where: { id }, data: changes });
@@ -37,11 +37,36 @@ async update(id, changes) {
 
 },
 
-async delete(id) {
+async deleteBoard(id) {
     // DELETE http://localhost:5432/api/boards/1
     // DELETE FROM "Board" WHERE id = 1;
     const deleted = await prisma.board.delete({ where: { id } });
     return deleted;
 
 },
+
+async createCard(boardID,newCard){
+    const created = await prisma.card.create({ data: newCard });
+    //const board = await prisma.board.findUnique({ where: { boardId } });
+    prisma.board.update({
+        where: { boardID },
+        data: { cards: { push : created} },
+    })
+    return created;
+},
+
+async findCardById(id){
+    const card = await prisma.card.findUnique({ where: { id } });
+    return card;
+},
+
+async updateCard (id, changes) {
+    const updated = await prisma.card.update({ where: { id }, data: changes });
+    return updated;
+},
+
+async deleteCard(id) {
+    const deleted = await prisma.card.delete({ where: { id } });
+    return deleted;
+}
 }
--- back-end/api/server.js
+++ back-end/api/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const { PrismaClient } = require('@prisma/client');
+const { findBoardById, updateBoard, deleteBoard, findBoards, createBoard, createCard, findCardById, updateCard, deleteCard } = require('./board-model-prisma');
+
 
 const prisma = new PrismaClient();
 const server = express();
@@ -15,15 +17,17 @@ server.get((req, res) => {
 });
 
 // [Get] /api/boards
-server.get('/api/boards', async (req, res, next) => {
+server.get('/api/boards', async (req, res, next) => { //get all boards
     const search = req.query;
     try {
-        const boards = await prisma.board.findMany({
-            where: search
-        });
-        if (boards.length) {
+        const boards = await findBoards(search);
+        if (boards.length) { //error handling
             res.json(boards);
-        } else {
+        }
+        else if(!boards.length){
+            next({ status: 404, message: 'No boards in database' });
+        }
+        else {
             next({ status: 404, message: 'No boards found match the search criteria' });
         }
     } catch (err) {
@@ -32,14 +36,12 @@ server.get('/api/boards', async (req, res, next) => {
 });
 
 // [Get] /api/boards/:id
-server.get('/api/boards/:id', async (req, res, next) => {
+server.get('/api/boards/:id', async (req, res, next) => { //get Board by ID
     const id = Number(req.params.id);
 
     try {
-        const board = await prisma.board.findUnique({
-            where: { id }
-        });
-        if (board) {
+        const board = await findBoardById(id);
+        if (board) { //error handling
             res.json(board);
         } else {
             next({ status: 404, message: `Board not found with ID ${id}` });
@@ -50,18 +52,16 @@ server.get('/api/boards/:id', async (req, res, next) => {
 });
 
 // [Post] /api/boards
-server.post('/api/boards', async (req, res, next) => {
+server.post('/api/boards', async (req, res, next) => { //create Board
     const newBoard = req.body;
     try {
-        const newBoardValid = (
+        const newBoardValid = ( //error handling
             newBoard.title !== undefined &&
             newBoard.type !== undefined &&
             newBoard.image !== undefined
         );
         if (newBoardValid) {
-            const created = await prisma.board.create({
-                data: newBoard
-            });
+            const created = await createBoard(newBoard);
             res.status(201).json(created);
         } else {
             next({ status: 422, message: 'title, type, and image are required fields' });
@@ -71,26 +71,21 @@ server.post('/api/boards', async (req, res, next) => {
     }
 });
 
-// [Put] /api/boards/:id
-server.put('/api/boards/:id', async (req, res, next) => {
+// [Put] /api/boards/:id       /api/boards/:boardId/cards { votes: }
+server.put('/api/boards/:id', async (req, res, next) => { //update Board
     const id = Number(req.params.id);
     const changes = req.body;
 
     try {
-        const board = await prisma.board.findUnique({
-            where: { id }
-        });
+        const board = await findBoardById(id);
 
-        const changedValid = (
+        const changedValid = ( //error handling
             changes.title !== undefined ||
             changes.type !== undefined ||
             changes.image !== undefined
         );
         if (board && changedValid) {
-            const updated = await prisma.board.update({
-                where: { id },
-                data: changes
-            });
+            const updated = await updateBoard(id, changes);
             res.json(updated);
         } else {
             next({ status: 422, message: "Invalid ID or changes" });
@@ -101,16 +96,12 @@ server.put('/api/boards/:id', async (req, res, next) => {
 });
 
 // [Delete] /api/boards/:id
-server.delete('/api/boards/:id', async (req, res, next) => {
+server.delete('/api/boards/:id', async (req, res, next) => { //delete Board
     const id = Number(req.params.id);
     try {
-        const board = await prisma.board.findUnique({
-            where: { id }
-        });
-        if (board) {
-            const deleted = await prisma.board.delete({
-                where: { id }
-            });
+        const board = await findBoardById(id);
+        if (board) { //error handling
+            const deleted = await deleteBoard(id);
             res.json(deleted);
         } else {
             next({ status: 404, message: `Board not found with ID ${id}` });
@@ -120,11 +111,119 @@ server.delete('/api/boards/:id', async (req, res, next) => {
     }
 });
 
+
+
+
+/*-------------------CARD ROUTES-------------------*/
+
+
+
+// [Get] /api/board/:boardId/cards
+server.get('/api/boards/:boardId/cards', async (req, res, next) => { //get all cards for a board
+    const boardId = Number(req.params.boardId);
+    const search = { ...req.query, boardId }; // Ensure boardId is part of the search criteria
+
+    try{
+        const cards = await prisma.card.findMany({
+            where: search
+        });
+        if(cards.length){ //error handling
+            res.json(cards);
+        }
+        else{
+            next({ status: 404, message: 'No cards found match the search criteria' });
+        }
+    }catch(err){
+        next(err);
+    }
+})
+
+// [Post] /api/board/:boardId/cards
+server.post('/api/boards/:boardId/cards', async (req, res, next) => { //create card in board
+    const boardId = Number(req.params.boardId);
+    const newCard = req.body;
+    newCard.boardId = boardId;
+    try{
+        const newCardValid = ( //error handling
+            newCard.title !== undefined &&
+            newCard.description !== undefined &&
+            newCard.gif !== undefined
+        );
+        if(newCardValid){
+            const created = await createCard(boardId, newCard);
+            res.status(201).json(created);
+        }
+        else{
+            next({ status: 422, message: 'title, description, and gif are required fields' });
+        }
+
+    }catch(err){
+        next(err);
+    }
+})
+
+
+//[Put] /api/board/:boardId/cards/:cardId
+server.put('/api/boards/:boardId/cards/:cardId', async (req, res, next) => { // update vote count
+    const boardId = Number(req.params.boardId);
+    const cardId = Number(req.params.cardId);
+    const changes = req.body;
+
+    try{
+        const board = await findBoardById(boardId);
+        const card = await findCardById(cardId);
+
+        if(board){
+            const changedValid = (changes.upvotes !== undefined)
+
+            if(card && changedValid){ //error handling
+                const updated = await updateCard(cardId, changes);
+                res.json(updated);
+            }
+            else{
+                next({ status: 422, message: "Invalid ID or changes" });
+            }
+        }
+        else{
+            next({ status: 404, message: "Board not found" });
+        }
+
+    }catch(err){
+        next(err);
+    }
+
+})
+
+
+
+//[Delete] /api/board/:boardId/cards/:cardId
+server.delete('/api/boards/:boardId/cards/:cardId', async (req, res, next) => { //delete card by ID
+    const boardId = Number(req.params.boardId);
+    const cardId = Number(req.params.cardId);
+
+    try{
+        board = await findBoardById(boardId);
+        card = await findCardById(cardId);
+
+        if(board && card){ //error handling
+            const deleted = await deleteCard(cardId);
+            res.json(deleted);
+        }
+        else{
+            next({ status: 404, message: "Board or Card not found" });
+        }
+    }catch(err){
+        next(err);
+    }
+})
+
 // Error handling middleware
 server.use((err, req, res, next) => {
     const { message, status = 500 } = err;
     console.log(message);
     res.status(status).json({ message }); // Unsafe in prod
 });
+
+
 
 module.exports = server;
